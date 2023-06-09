0b3aaa707028ececa02f1d6972b4d3e7ac63890d
Javier Martín Gómez <j.martingomez@edu.gva.es>
2023-06-09 19:31:45 +0200

    cambios avances en routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import './App.css';
 import ListOfGifs from './components/ListOfGifs';
 
+import {Route} from "wouter";
+
 function App() {
   
   
   return (
     <div className="App">
       <section className="App-content">
-          <ListOfGifs keywords='hormiga' />
+        <h1>Aplicación de gifs</h1>
+        <a href='/gif/panda'>Gifs de pandas</a>
+        <a href='/gif/cacas'>Gifs de futbol</a>
+        <a href='/gif/naruto'>Gifs de naruto</a>
+        <Route 
+          component ={ListOfGifs}
+          path="/gif/:keywords"
+        />
       </section>
     </div>
   );
